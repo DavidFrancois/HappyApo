ebd2a1bd6ab59d2982c74c630f34b6a628922501
DavidFrancois <francois.david@aol.com>
2018-10-21 17:39:57 +0200

    [PERSISTENCY] Init Pers script

---
+++ Assets/DataManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataManager : MonoBehaviour {
+
+    public static DataManager dataManager;
+
+    bool gameStart;
+
+    private void Awake()
+    {
+        if (!gameStart) {
+            dataManager = this;
+            SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+            gameStart = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/load.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class load : MonoBehaviour {
+
+    public int scene;
+    bool loaded;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("HERE", collider);
+
+        if (!loaded) {
+            SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            loaded = true;
+        }
+    }
+}
