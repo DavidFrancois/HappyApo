1d23a2f5f4a4b0f983b66e4d175284a581b3c0c6
Falc0w <32669053+Falc0w@users.noreply.github.com>
2018-11-12 16:44:23 +0100

    Ennemy
    Commit Flo: Ennemy
    
    Co-Authored-By: falc0w <falc0w@users.noreply.github.com>

---
+++ Assets/Script/DeplacementEnnemi.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+public var vitesseDeplacement : float;
+public var deplacementDroite : boolean; 
+
+var LeMurOk : Transform;
+var LeMurRadius : float;
+var leMur : LayerMask;
+private var toucheLeMur : boolean;
+
+private var toucheLeVide : boolean;
+var LeVideOk : Transform;
+
+
+function Start () {
+
+
+}
+
+function Update () {
+
+	toucheLeMur = Physics2D.OverlapCircle(LeMurOk.position,LeMurRadius,leMur);
+	toucheLeVide = Physics2D.OverlapCircle(LeVideOk.position,LeMurRadius,leMur);
+
+	if(toucheLeMur || !toucheLeVide)
+	{
+		deplacementDroite = !deplacementDroite;
+	}
+
+
+
+	if(deplacementDroite)
+	{
+		transform.localScale = new Vector3 (-2f, 2.5f, 1f);
+		GetComponent.<Rigidbody2D>().velocity = new Vector2(vitesseDeplacement,GetComponent.<Rigidbody2D>().velocity.y);
+	}else {
+		transform.localScale = new Vector3 (2f, 2.5f, 1f);
+		GetComponent.<Rigidbody2D>().velocity = new Vector2(-vitesseDeplacement,GetComponent.<Rigidbody2D>().velocity.y);
+	}
+
+}
